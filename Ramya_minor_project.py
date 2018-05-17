15abc7d8f92179f08d41c3b0fe79feee2ff9424d
tejaAPI <brahmateja.bollineni@gmail.com>
2018-05-17 13:27:18 +0530

    comments added

--- Ramya_minor_project.py
+++ Ramya_minor_project.py
@@ -1,11 +1,13 @@
+#sorting the one lakh numbers 
+#importing the random module
 import random
 l=[]
 count=0
 print("Generating the random numbers and appending into the list.......")
-
+#Generating the random numbers
 while(count!=10000):
 	x=random.randint(0,10000)
-	if x not in l:
+	if x not in l:#checking the number repititions
 		l.append(x)
 		count+=1
 		print(x,"is appended....")
@@ -14,7 +16,7 @@ print("Entering the numbers from the list into the file without sorting.......")
 with open("num.txt",'w') as f:
 	f.write("********Before Sorting**********\n")
 	f.write(str(l))
-l.sort()
+l.sort()#applying the sort method 
 print("Entering the data into the file after sorting.......")
 with open("numsort.txt",'w') as s:
 	s.write("*********After Sorting***********\n")
